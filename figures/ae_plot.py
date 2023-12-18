0439628614022ab45435e1eb0078c963fb283f3a
Shuo HAO <shuohaopolyu@gmail.com>
2023-12-18 19:20:25 +0800

    update on experiment 1

--- figures/ae_plot.py
+++ figures/ae_plot.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 
 def num_hid_neuron():
-
     with open("./dataset/ae_num_hid_neuron.pkl", "rb") as f:
         data = pickle.load(f)
 
@@ -15,74 +14,144 @@ def num_hid_neuron():
     error_5 = data["error_velo_test_1"]
     error_6 = data["error_velo_test_2"]
 
-    plt.plot(np.arange(1, 15), error_1[1:, 2], label="error_disp_train", marker="o", color="blue")
-    plt.plot(np.arange(1, 15), error_2[1:, 2], label="error_disp_test_1", marker="s", color="blue", linestyle="dashed")
-    plt.plot(np.arange(1, 15), error_3[1:, 2], label="error_disp_test_2", marker="^", color="blue", linestyle="-.")
-    plt.plot(np.arange(1, 15), error_4[1:, 2], label="error_velo_train", marker="D", color="red")
-    plt.plot(np.arange(1, 15), error_5[1:, 2], label="error_velo_test_1", marker="v", color="red", linestyle="dashed")
-    plt.plot(np.arange(1, 15), error_6[1:, 2], label="error_velo_test_2", marker="P", color="red", linestyle="-.")
+    plt.plot(
+        np.arange(1, 15),
+        error_1[1:, 2],
+        label="Displacement: Train",
+        marker="o",
+        color="dimgrey",
+    )
+    plt.plot(
+        np.arange(1, 15),
+        error_2[1:, 2],
+        label="Displacement: Test 1",
+        marker="s",
+        color="dimgrey",
+        linestyle="dashed",
+    )
+    plt.plot(
+        np.arange(1, 15),
+        error_3[1:, 2],
+        label="Displacement: Test 2",
+        marker="^",
+        color="dimgrey",
+        linestyle="-.",
+    )
+    plt.plot(
+        np.arange(1, 15),
+        error_4[1:, 2],
+        label="Velocity: Train",
+        marker="D",
+        color="darkred",
+    )
+    plt.plot(
+        np.arange(1, 15),
+        error_5[1:, 2],
+        label="Velocity: Test 1",
+        marker="v",
+        color="darkred",
+        linestyle="dashed",
+    )
+    plt.plot(
+        np.arange(1, 15),
+        error_6[1:, 2],
+        label="Velocity: Test 2",
+        marker="P",
+        color="darkred",
+        linestyle="-.",
+    )
     plt.yscale("log")
-
+    plt.xlim(1, 14)
+    plt.ylim(1e-9, 1e-2)
+    plt.tick_params(axis="both", direction="in", which="both", top=True, right=True)
+    plt.xticks(np.arange(1, 15, 1))
     plt.legend()
-    plt.xlabel("Number of neurons in hidden layer")
+    plt.xlabel("Number of Hidden Neurons")
     plt.ylabel("Mean Saure Error")
-    plt.savefig("./figures/num_hid_neuron.png")
-    plt.show()
+    plt.tight_layout()
+    plt.savefig("./figures/num_hid_neuron.png", dpi=300)
+    # plt.show()
+
 
 def data_size():
     # needs modification
     with open("./dataset/ae_train_data_size.pkl", "rb") as f:
         data = pickle.load(f)
     error_disp_train_mean = data["error_disp_train_mean"]
-    error_disp_train_max = data["error_disp_train_max"]
-    error_disp_train_min = data["error_disp_train_min"]
-    error_disp_train_std = data["error_disp_train_std"]
     error_disp_test_1_mean = data["error_disp_test_1_mean"]
-    error_disp_test_1_max = data["error_disp_test_1_max"]
-    error_disp_test_1_min = data["error_disp_test_1_min"]
-    error_disp_test_1_std = data["error_disp_test_1_std"]
     error_disp_test_2_mean = data["error_disp_test_2_mean"]
-    error_disp_test_2_max = data["error_disp_test_2_max"]
-    error_disp_test_2_min = data["error_disp_test_2_min"]
-    error_disp_test_2_std = data["error_disp_test_2_std"]
     error_velo_train_mean = data["error_velo_train_mean"]
-    error_velo_train_max = data["error_velo_train_max"]
-    error_velo_train_min = data["error_velo_train_min"]
     error_velo_test_1_mean = data["error_velo_test_1_mean"]
-    error_velo_test_1_max = data["error_velo_test_1_max"]
-    error_velo_test_1_min = data["error_velo_test_1_min"]
     error_velo_test_2_mean = data["error_velo_test_2_mean"]
-    error_velo_test_2_max = data["error_velo_test_2_max"]
-    error_velo_test_2_min = data["error_velo_test_2_min"]
-    error_velo_test_2_std = data["error_velo_test_2_std"]
 
     num_data = np.array([20, 50, 100, 200, 500, 1000, 2000, 4000, 8000])
-    
+
     # fill the interval between max and min
-    plt.plot(num_data, error_disp_train_mean[:, 2], label="error_disp_train", marker="o", color="blue")
-    # plt.fill_between(num_data, error_disp_train_min[:, 2], error_disp_train_max[:, 2], alpha=0.2, color="blue")
-    plt.plot(num_data, error_disp_test_1_mean[:, 2], label="error_disp_test_1", marker="s", color="blue", linestyle="dashed")
-    # plt.fill_between(num_data, error_disp_test_1_min[:, 2], error_disp_test_1_max[:, 2], alpha=0.2, color="blue")
-    plt.plot(num_data, error_disp_test_2_mean[:, 2], label="error_disp_test_2", marker="^", color="blue", linestyle="-.")
-    # plt.fill_between(num_data, error_disp_test_2_min[:, 2], error_disp_test_2_max[:, 2], alpha=0.2, color="blue")
-    plt.plot(num_data, error_velo_train_mean[:, 2], label="error_velo_train", marker="D", color="red")
-    # plt.fill_between(num_data, error_velo_train_min[:, 2], error_velo_train_max[:, 2], alpha=0.2, color="red")
-    plt.plot(num_data, error_velo_test_1_mean[:, 2], label="error_velo_test_1", marker="v", color="red", linestyle="dashed")
-    # plt.fill_between(num_data, error_velo_test_1_min[:, 2], error_velo_test_1_max[:, 2], alpha=0.2, color="red")
-    plt.plot(num_data, error_velo_test_2_mean[:, 2], label="error_velo_test_2", marker="P", color="red", linestyle="-.")
-    # plt.fill_between(num_data, error_velo_test_2_min[:, 2], error_velo_test_2_max[:, 2], alpha=0.2, color="red")
+    plt.plot(
+        num_data,
+        error_disp_train_mean[:, 2],
+        marker="o",
+        color="dimgrey",
+        label="Displacement: Train",
+    )
+    plt.plot(
+        num_data,
+        error_disp_test_1_mean[:, 2],
+        marker="s",
+        color="dimgrey",
+        linestyle="dashed",
+        label="Displacement: Test 1",
+    )
+    plt.plot(
+        num_data,
+        error_disp_test_2_mean[:, 2],
+        marker="^",
+        color="dimgrey",
+        linestyle="-.",
+        label="Displacement: Test 2",
+    )
+    plt.plot(
+        num_data,
+        error_velo_train_mean[:, 2],
+        marker="D",
+        color="darkred",
+        label="Velocity: Train",
+    )
+    plt.plot(
+        num_data,
+        error_velo_test_1_mean[:, 2],
+        marker="v",
+        color="darkred",
+        linestyle="dashed",
+        label="Velocity: Test 1",
+    )
+    plt.plot(
+        num_data,
+        error_velo_test_2_mean[:, 2],
+        marker="P",
+        color="darkred",
+        linestyle="-.",
+        label="Velocity: Test 2",
+    )
     plt.yscale("log")
     # print(error_disp_test_1_mean[:, 2] - error_disp_test_1_std[:, 2])
     plt.yscale("log")
     plt.xscale("log")
-    plt.ylim(1e-9, 1e-4)
+    plt.ylim(1e-9, 1e-3)
     plt.xlim(20, 8000)
+    # plt.xticks([20, 50, 100, 200, 500, 1000, 2000, 4000, 8000])
+    plt.tick_params(axis="both", direction="in", which="both", top=True, right=True)
     plt.legend()
-    plt.xlabel("Number of training data")
+    plt.xlabel("Number of Training Data")
     plt.ylabel("Mean Saure Error")
-    plt.savefig("./figures/data_size.png")
-    plt.show()
+    plt.tight_layout()
+    plt.savefig("./figures/data_size.png", dpi=300)
+    # plt.show()
+
 
 if __name__ == "__main__":
-    # num_hid_neuron()
+    # set the font family
+    plt.rc("font", family="serif")
+    plt.rc("font", size=12)
+    num_hid_neuron()
     data_size()
